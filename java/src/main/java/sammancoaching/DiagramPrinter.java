76de7dda1fb6295319fa60eb4231dffdba58b2c8
ibanFR <ibanFR@users.noreply.github.com>
2025-06-28 08:05:21 +0100

    refactor: extract method getSummaryInformation from printSummary

--- java/src/main/java/sammancoaching/DiagramPrinter.java
+++ java/src/main/java/sammancoaching/DiagramPrinter.java
@@ -25,10 +25,15 @@ public class DiagramPrinter {
     private static boolean printSummary(DiagramWrapper wrapper, String language, StringBuilder summaryText) {
         DiagramSummary summary = new DiagramSummary(language);
         summary.addTitle(wrapper.getName(), wrapper.getSerialNumber());
-        summary.addHeader(wrapper.diagram().getSummaryInformation());
+        summary.addHeader(getSummaryInformation(wrapper));
         summary.addImage(wrapper.diagram().getFlowchartThumbnail());
         summaryText.append(summary.export());
         return true;
+    }
+
+    private static String getSummaryInformation(DiagramWrapper wrapper) {
+        return wrapper.diagram()
+                .getSummaryInformation();
     }
 
     public boolean printDiagram(FlowchartDiagram diagram, String folder, String filename) throws IOException {
